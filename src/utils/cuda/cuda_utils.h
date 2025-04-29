ab58bc6e5bbaa5fe53e73b72273c9d0cd06ec729
Artem Y. Polyakov <artemp@nvidia.com>
2025-04-29 10:44:51 -0400

    utils: Introduce CUDA util component & cleanup meson infra
    Abstract CUDA functionality leveraged by some of the
    backends, specifically, UCX.
    
    The CUDA util comonent gets build regardless of CUDA presense
    and allows to focus all CUDA-related logic and complexity
    within one place.
    
    Also optimize some of the meson build files to expose
    and propagate internal component dependencies.
    
    Signed-off-by: Artem Y. Polyakov <artemp@nvidia.com>

---
+++ src/utils/cuda/cuda_utils.h
@@ -0,0 +1,66 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* forward declaration for the internal CUDA data structure */
+
+#include <nixl.h>
+#include <memory>
+
+/****************************************
+ * Pointer Context
+*****************************************/
+
+namespace nixlCuda {
+    class memCtx {
+    public:
+        enum memory_t {
+            MEM_NONE,
+            MEM_HOST,
+            MEM_DEV,
+            MEM_VMM_DEV,
+        } ;
+    protected:
+        memory_t memType;
+        uint64_t _devId;
+    public:
+        memCtx() : memType(MEM_NONE), _devId(0)
+        {  }
+
+        virtual ~memCtx() = default;
+
+        memory_t getMemType() const {
+            return memType;
+        }
+
+        uint64_t getDevId() const {
+            return _devId;
+        }
+
+        virtual nixl_status_t set() {
+            // no-op for non-CUDA case
+            return NIXL_SUCCESS;
+        }
+
+        virtual nixl_status_t enableAddr(const void *address, uint64_t chkDevId) {
+            return NIXL_SUCCESS;
+        }
+
+        static std::unique_ptr<memCtx> memCtxInit();
+    };
+
+    uint32_t numDevices();
+}
